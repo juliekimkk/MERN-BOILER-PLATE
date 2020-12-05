2fdd589c8a0ad630afc85a8b8627412da32fbb1a
plusmail <dbr01075@naver.com>
2020-12-05 19:07:10 +0900

    비밀번호 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    mongoURI: process.env.MONGO_URI //헤로쿠에서 똑같이 이름해줘야함
+}
---
+++ models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+//user에 save하기전에 해주고 save로 넘어감
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    //비밀번호를 암호화 시킨다.
+
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+        bcrypt.hash(user.password , salt, function(err, hash) {
+            // Store hash in  your password DB.
+        });
+    });
+
+    next()
+})
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
